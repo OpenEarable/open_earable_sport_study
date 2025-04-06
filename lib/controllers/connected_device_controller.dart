0b735f9a0ea220b347b911bc03212ed4ae0a6534
Jonas Greifenhain <cadivus@daverkomp.de>
2025-04-06 16:28:54 +0200

    1

--- lib/controllers/connected_device_controller.dart
+++ lib/controllers/connected_device_controller.dart
@@ -1,12 +1,13 @@
 import 'dart:async';
 import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_foreground_task/task_handler.dart';
 import 'package:open_earable_flutter/open_earable_flutter.dart';
 
 import '../models/auto_connect_device.dart';
 import 'settings_controller.dart';
 
-class ConnectedDeviceController extends ChangeNotifier {
+class ConnectedDeviceController extends ChangeNotifier implements TaskHandler {
   late final SettingsController _settingsController;
 
   // Wearable management
@@ -76,55 +77,7 @@ class ConnectedDeviceController extends ChangeNotifier {
 
     _settingsController.addListener(_onSettingsChanged);
 
-    // Listen for new connected devices
-    _wearableManager.connectStream.listen((wearable) {
-      wearable.addDisconnectListener(() {
-        if (connectedDevices
-            .any((device) => device.deviceId == wearable.deviceId)) {
-          connectedDevices
-              .removeWhere((device) => device.deviceId == wearable.deviceId);
-          notifyListeners();
-        }
-      });
 
-      // Enable every sensor
-      if (wearable is SensorManager) {
-        for (Sensor sensor in (wearable as SensorManager).sensors) {
-          for (SensorConfiguration config in sensor.relatedConfigurations) {
-            if (config is SensorFrequencyConfiguration) {
-              config.setMaximumFrequency();
-            }
-          }
-
-          // Listen for heart rate updates
-          if (sensor is HeartRateSensor) {
-            StreamSubscription hrs = sensor.sensorStream.listen((heartRate) {
-              print("Heart rate: ${heartRate.heartRateBpm} | ${DateTime.now().toIso8601String()}");
-              _updateHeartRate(wearable.deviceId, heartRate.heartRateBpm);
-            });
-            wearable.addDisconnectListener(() {
-              hrs.cancel();
-            });
-          }
-        }
-      }
-
-      connectingDevices.removeWhere((d) => d.id == wearable.deviceId);
-      connectedDevices.add(wearable);
-      notifyListeners();
-
-      for (final callback in _onConnectCallbacks) {
-        callback(wearable);
-      }
-    });
-
-    // Listen for new connecting devices
-    _wearableManager.connectingStream.listen((device) {
-      connectingDevices.add(device);
-      notifyListeners();
-    });
-
-    startScanning();
   }
 
   void _onSettingsChanged() {
@@ -184,6 +137,92 @@ class ConnectedDeviceController extends ChangeNotifier {
     _heartRateResetTimer?.cancel();
     _heartRateStreamController.close();
     super.dispose();
+  }
+
+  @override
+  Future<void> onDestroy(DateTime timestamp) {
+    // TODO: implement onDestroy
+    throw UnimplementedError();
+  }
+
+  @override
+  void onNotificationButtonPressed(String id) {
+    // TODO: implement onNotificationButtonPressed
+  }
+
+  @override
+  void onNotificationDismissed() {
+    // TODO: implement onNotificationDismissed
+  }
+
+  @override
+  void onNotificationPressed() {
+    // TODO: implement onNotificationPressed
+  }
+
+  @override
+  void onReceiveData(Object data) {
+    // TODO: implement onReceiveData
+  }
+
+  @override
+  void onRepeatEvent(DateTime timestamp) {
+    // TODO: implement onRepeatEvent
+  }
+
+  @override
+  Future<void> onStart(DateTime timestamp, TaskStarter starter) async {
+    print("=====> onStart");
+
+    // Listen for new connected devices
+    _wearableManager.connectStream.listen((wearable) {
+      wearable.addDisconnectListener(() {
+        if (connectedDevices
+            .any((device) => device.deviceId == wearable.deviceId)) {
+          connectedDevices
+              .removeWhere((device) => device.deviceId == wearable.deviceId);
+          notifyListeners();
+        }
+      });
+
+      // Enable every sensor
+      if (wearable is SensorManager) {
+        for (Sensor sensor in (wearable as SensorManager).sensors) {
+          for (SensorConfiguration config in sensor.relatedConfigurations) {
+            if (config is SensorFrequencyConfiguration) {
+              config.setMaximumFrequency();
+            }
+          }
+
+          // Listen for heart rate updates
+          if (sensor is HeartRateSensor) {
+            StreamSubscription hrs = sensor.sensorStream.listen((heartRate) {
+              print("Heart rate: ${heartRate.heartRateBpm} | ${DateTime.now().toIso8601String()}");
+              _updateHeartRate(wearable.deviceId, heartRate.heartRateBpm);
+            });
+            wearable.addDisconnectListener(() {
+              hrs.cancel();
+            });
+          }
+        }
+      }
+
+      connectingDevices.removeWhere((d) => d.id == wearable.deviceId);
+      connectedDevices.add(wearable);
+      notifyListeners();
+
+      for (final callback in _onConnectCallbacks) {
+        callback(wearable);
+      }
+    });
+
+    // Listen for new connecting devices
+    _wearableManager.connectingStream.listen((device) {
+      connectingDevices.add(device);
+      notifyListeners();
+    });
+
+    startScanning();
   }
 }
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_foreground_task/flutter_foreground_task.dart';
 
 import 'controllers/connected_device_controller.dart';
 import 'controllers/settings_controller.dart';
@@ -7,35 +10,53 @@ import 'controllers/recording_controller.dart';
 import 'pages/home_page.dart';
 import 'pages/recording_page.dart';
 
+SettingsController? _settingsController;
+ConnectedDeviceController? _connectedDeviceController;
+RecordingController? _recordingController;
+
+// The callback function should always be a top-level or static function.
+@pragma('vm:entry-point')
+void startCallback() {
+
+  _settingsController = SettingsController();
+  _connectedDeviceController = ConnectedDeviceController(
+    settingsController: _settingsController!,
+  );
+  _recordingController = RecordingController(
+    deviceController: _connectedDeviceController!,
+    settingsController: _settingsController!,
+  );
+
+  print(" ====== > startCallback");
+  FlutterForegroundTask.setTaskHandler(_connectedDeviceController!);
+}
+
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  FlutterForegroundTask.initCommunicationPort();
+
+  print(" ====== > main");
   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider<SettingsController>(
-          create: (_) => SettingsController(),
+          create: (_) {
+            return _settingsController!;
+          },
         ),
         ChangeNotifierProxyProvider<SettingsController,
             ConnectedDeviceController>(
-          create: (context) => ConnectedDeviceController(
-            settingsController: Provider.of<SettingsController>(
-              context,
-              listen: false,
-            ),
-          ),
+          create: (context) {
+            return _connectedDeviceController!;
+          },
           update: (context, settingsController, connectedDeviceController) =>
               connectedDeviceController!,
         ),
         ChangeNotifierProvider<RecordingController>(
-          create: (context) => RecordingController(
-            deviceController: Provider.of<ConnectedDeviceController>(
-              context,
-              listen: false,
-            ),
-            settingsController: Provider.of<SettingsController>(
-              context,
-              listen: false,
-            ),
-          ),
+          create: (context) {
+            return _recordingController!;
+          },
         ),
       ],
       child: const MyApp(),
@@ -43,8 +64,99 @@ void main() {
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      // Request permissions and initialize the service.
+      await _requestPermissions();
+      _initService();
+      await _startService();
+    });
+  }
+
+  void _initService() {
+    print(" ====== > _initService");
+    FlutterForegroundTask.init(
+      androidNotificationOptions: AndroidNotificationOptions(
+        channelId: 'foreground_service',
+        channelName: 'Foreground Service Notification',
+        channelDescription:
+            'This notification appears when the foreground service is running.',
+        onlyAlertOnce: true,
+      ),
+      iosNotificationOptions: const IOSNotificationOptions(
+        showNotification: false,
+        playSound: false,
+      ),
+      foregroundTaskOptions: ForegroundTaskOptions(
+        eventAction: ForegroundTaskEventAction.repeat(5000),
+        autoRunOnBoot: true,
+        autoRunOnMyPackageReplaced: true,
+        allowWakeLock: true,
+        allowWifiLock: true,
+      ),
+    );
+  }
+
+  Future<ServiceRequestResult> _startService() async {
+    if (await FlutterForegroundTask.isRunningService) {
+      return FlutterForegroundTask.restartService();
+    } else {
+      return FlutterForegroundTask.startService(
+        serviceId: 256,
+        notificationTitle: 'Foreground Service is running',
+        notificationText: 'Tap to return to the app',
+        notificationIcon: null,
+        notificationButtons: [
+          const NotificationButton(id: 'btn_hello', text: 'hello'),
+        ],
+        notificationInitialRoute: '/',
+        callback: startCallback,
+      );
+    }
+  }
+
+  Future<void> _requestPermissions() async {
+    print(" ====== > _requestPermissions");
+    // Android 13+, you need to allow notification permission to display foreground service notification.
+    //
+    // iOS: If you need notification, ask for permission.
+    final NotificationPermission notificationPermission =
+        await FlutterForegroundTask.checkNotificationPermission();
+    if (notificationPermission != NotificationPermission.granted) {
+      await FlutterForegroundTask.requestNotificationPermission();
+    }
+
+    if (Platform.isAndroid) {
+      // Android 12+, there are restrictions on starting a foreground service.
+      //
+      // To restart the service on device reboot or unexpected problem, you need to allow below permission.
+      if (!await FlutterForegroundTask.isIgnoringBatteryOptimizations) {
+        // This function requires `android.permission.REQUEST_IGNORE_BATTERY_OPTIMIZATIONS` permission.
+        await FlutterForegroundTask.requestIgnoreBatteryOptimization();
+      }
+
+      // Use this utility only if you provide services that require long-term survival,
+      // such as exact alarm service, healthcare service, or Bluetooth communication.
+      //
+      // This utility requires the "android.permission.SCHEDULE_EXACT_ALARM" permission.
+      // Using this permission may make app distribution difficult due to Google policy.
+      // if (!await FlutterForegroundTask.canScheduleExactAlarms) {
+      //   // When you call this function, will be gone to the settings page.
+      //   // So you need to explain to the user why set it.
+      //   await FlutterForegroundTask.openAlarmsAndRemindersSettings();
+      // }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
